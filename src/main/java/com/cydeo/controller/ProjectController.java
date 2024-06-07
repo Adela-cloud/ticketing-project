2f9001fcaede78a947104926d98e072d52bc7b17
Adela-cloud <dilov222@gmail.com>
2024-06-07 18:36:17 +0300

    create project  and delete project functions are done.

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,33 +1,43 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
-    private final RoleService  roleService;
     private final UserService userService;
     private final ProjectService projectService;
 
-    public ProjectController(RoleService roleService, UserService userService, ProjectService projectService) {
-        this.roleService = roleService;
+    public ProjectController(UserService userService, ProjectService projectService) {
         this.userService = userService;
         this.projectService = projectService;
     }
 
     @GetMapping("/create")
-    public String createProject(Model model){
+    public String createProject(Model model) {
         model.addAttribute("project", new ProjectDTO());
         model.addAttribute("projects", projectService.findAll());
         model.addAttribute("managers", userService.findAll());
 
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String saveProject(@ModelAttribute("project") ProjectDTO project) {
+        projectService.save(project);
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete{projectCode}")
+    public String deleteProject(@PathVariable("projectCode") String code) {
+        projectService.deleteById(code);
+        return "redirect:/project/create";
     }
 }
