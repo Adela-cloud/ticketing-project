53924260d3b66d45305d24d6a61c229ad16197ff
Adela-cloud <dilov222@gmail.com>
2024-06-08 14:38:37 +0300

    update and complete project functions are done, but there is error of "cannot find template project/project/project-create

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,9 +1,9 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
-import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+
     private final UserService userService;
     private final ProjectService projectService;
 
@@ -22,22 +23,59 @@ public class ProjectController {
 
     @GetMapping("/create")
     public String createProject(Model model) {
+
         model.addAttribute("project", new ProjectDTO());
+
+        model.addAttribute("managers", userService.findManagers());
+
         model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("managers", userService.findAll());
 
         return "/project/create";
     }
 
     @PostMapping("/create")
-    public String saveProject(@ModelAttribute("project") ProjectDTO project) {
+    public String insertProject(@ModelAttribute("project") ProjectDTO project) {
+
+
         projectService.save(project);
+
         return "redirect:/project/create";
     }
 
-    @GetMapping("/delete{projectCode}")
-    public String deleteProject(@PathVariable("projectCode") String code) {
-        projectService.deleteById(code);
+    @GetMapping("/delete/{projectCode}")
+    public String deleteProject(@PathVariable("projectCode") String projectCode) {
+
+        projectService.deleteById(projectCode);
+
+        return "redirect:/project/create";
+
+    }
+
+    @GetMapping("/complete/{projectCode}")
+    public String completeProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.complete(projectService.findById(projectCode));
+
         return "redirect:/project/create";
     }
+
+    @GetMapping("/update/{projectCode}")
+    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
+
+        model.addAttribute("project",projectService.findById(projectCode));
+        model.addAttribute("managers",userService.findManagers());
+        model.addAttribute("projects",projectService.findAll());
+
+        return "/project/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProject(@ModelAttribute("project") ProjectDTO project){
+
+        projectService.update(project);
+
+        return "redirect:/project/create";
+    }
+
+
 }
